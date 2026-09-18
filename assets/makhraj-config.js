6db091a4f549de1eb128151a6c10d5a91994435d
Yazzeedssrr <fdbk8mswbd@privaterelay.appleid.com>
2026-09-18 12:23:54 -0400

    Add Makhraj production Supabase config

---
+++ assets/makhraj-config.js
@@ -0,0 +1,7 @@
+window.MAKHRAJ_CONFIG = Object.freeze({
+  supabaseUrl: "https://fskfwngswatbetgxkmei.supabase.co",
+  supabaseKey: "sb_publishable_9G0mBOEj5411ERnEP2Gv_g_76rMoMzt",
+  currency: "USD",
+  freeShippingThreshold: 75,
+  standardShipping: 6
+});
