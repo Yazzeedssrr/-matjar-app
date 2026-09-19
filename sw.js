8ced53cd59279669851983339114bf85231ca3d3
Yazzeedssrr <fdbk8mswbd@privaterelay.appleid.com>
2026-09-19 12:29:51 -0400

    Bump revenue cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='makhraj-prod-v9-revenue1';
+const CACHE='makhraj-prod-v10-revenue2';
 const SHELL=[
   './app.html','./manifest.webmanifest','./assets/makhraj-theme.css','./assets/makhraj-app.css',
   './assets/makhraj-config.js','./assets/makhraj-i18n.js','./assets/makhraj-app.js','./assets/makhraj-need.js','./assets/makhraj-bundles.js'
